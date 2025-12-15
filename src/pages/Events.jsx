a7267a587014fabe84728e81fcd53bb4c421d9ca
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-12-15 16:30:08 +0600

    added search and filtering functionality on all events page

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Search, Filter } from "lucide-react";
 import useAxiosSecure from "../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
@@ -7,15 +7,21 @@ import EventCard from "./dashboard/clubManager/EventCard";
 
 const Events = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: events, isLoading } = useQuery({
-    queryKey: ["events"],
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [searchText, setSearchText] = useState("");
+  const {
+    data: events = [],
+    isLoading,
+    isFetching,
+  } = useQuery({
+    queryKey: ["events", selectedCategory, searchText],
     queryFn: async () => {
-      const res = await axiosSecure.get("/getEvents");
+      const res = await axiosSecure.get(
+        `/filteredEvents?category=${selectedCategory}&search=${searchText}`
+      );
       return res.data;
     },
   });
-
-  if (isLoading) return <div className="min-h-screen"><Loading /></div>;
 
   const categories = [
     "Photography",
@@ -55,7 +61,9 @@ const Events = () => {
           <Search className="absolute left-6 top-1/2 -translate-y-1/2 w-6 h-6 text-gray-400" />
           <input
             type="text"
-            placeholder="Search by club name..."
+            placeholder="Search by event name..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
             className="w-full pl-16 pr-6 py-5 text-lg bg-white border border-gray-300 rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-main/30 focus:border-main transition"
           />
         </div>
@@ -64,25 +72,24 @@ const Events = () => {
         <div className="flex flex-col lg:flex-row justify-between items-start  gap-6">
           {/* Categories */}
           <div className="flex flex-wrap gap-3">
-            {categories.map((cat) => (
-              <button
-                key={cat}
-                className={`px-6 py-3 rounded-full font-medium transition-all ${
-                  cat === "All"
-                    ? "bg-main text-white shadow-lg"
-                    : "bg-white text-gray-700 border border-gray-300 hover:border-main hover:shadow-md"
-                }`}
-              >
-                {cat}
-              </button>
-            ))}
+            <p className="font-bold text-2xl border-l-8 pl-3 border-main">
+              <span className="text-main">{events?.length}</span> Events Found
+            </p>
           </div>
 
           {/* Sort Dropdown */}
           <div className="flex items-center gap-3">
-            <select className="select select-lg px-8 py-3  bg-white border border-gray-300 rounded-full font-medium text-gray-700 focus:outline-none focus:ring-2 focus:ring-main/30 cursor-pointer shadow-sm">
-              <option>Newest First</option>
-              <option>Oldest First</option>
+            <select
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+              className="select select-lg px-8 py-3 bg-white border border-gray-300 rounded-full font-medium text-gray-700 cursor-pointer shadow-sm"
+            >
+              <option value="all">All Categories</option>
+              {categories.map((category, index) => (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              ))}
             </select>
           </div>
         </div>
@@ -90,27 +97,28 @@ const Events = () => {
 
       {/* Clubs  */}
       <div className="w-11/12 mx-auto px-4 pb-20">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-          {events?.map((event) => (
-            <EventCard key={event._id} event={event} />
-          ))}
-        </div>
-
-        {/*  if no events*/}
-        {events?.length === 0 && (
-          <>
-            <div className="text-center py-20">
-              <div className="w-32 h-32 bg-gray-200 rounded-full mx-auto mb-6 flex items-center justify-center">
-                <Search className="w-16 h-16 text-gray-400" />
-              </div>
-              <h3 className="text-2xl font-bold text-gray-700">
-                No events found
-              </h3>
-              <p className="text-gray-500 mt-3">
-                Try adjusting your filters or search term.
-              </p>
+        {isLoading || isFetching ? (
+          <div className="flex justify-center items-center py-20">
+            <Loading />
+          </div>
+        ) : events.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+            {events.map((event) => (
+              <EventCard key={event._id} event={event} />
+            ))}
+          </div>
+        ) : (
+          <div className="text-center py-20">
+            <div className="w-32 h-32 bg-gray-200 rounded-full mx-auto mb-6 flex items-center justify-center">
+              <Search className="w-16 h-16 text-gray-400" />
             </div>
-          </>
+            <h3 className="text-2xl font-bold text-gray-700">
+              No events found
+            </h3>
+            <p className="text-gray-500 mt-3">
+              Try adjusting your filters or search term.
+            </p>
+          </div>
         )}
       </div>
     </div>
