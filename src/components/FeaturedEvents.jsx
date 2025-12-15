860eb874414141ff3c3d0bff97d876ed85c642d3
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-12-15 17:12:00 +0600

    added fetarued events section

--- src/components/FeaturedEvents.jsx
+++ src/components/FeaturedEvents.jsx
@@ -2,39 +2,39 @@ import React from "react";
 import Container from "./Container";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../hooks/useAxiosSecure";
-import ClubCard from "../pages/dashboard/clubManager/ClubCard";
 import Loading from "./animation/Loading";
 import { Link } from "react-router";
+import EventCard from "../pages/dashboard/clubManager/EventCard";
 
-const FeaturedClubs = () => {
+const FeaturedEvents = () => {
   const axios = useAxiosSecure();
-  const { data: Clubs, isLoading } = useQuery({
-    queryKey: ["allClubs"],
+  const { data: Events, isLoading } = useQuery({
+    queryKey: ["events"],
     queryFn: async () => {
-      const res = await axios.get("/clubs?status=approved");
+      const res = await axios.get("/getEvents");
       return res.data;
     },
   });
    
 
   if (isLoading) return <div className="min-h-screen"><Loading /></div>;
-  const allClubs = Clubs?.slice(0, 5);
-   console.log(allClubs);
+  const allEvents = Events?.slice(0, 5);
+   console.log(allEvents);
   return (
     <Container>
-      <div className="my-16">
+      <div className="my-20">
         <div className="flex justify-between">
-          <h3 className="font-bold sm:text-4xl text-2xl">Clubs You Might Like</h3>
-          <Link to='/clubs'>
+          <h3 className="font-bold sm:text-4xl text-2xl">Events from Different Clubs</h3>
+          <Link to='/events'>
           <button className="rounded-xl transform  transition text-lg border-2 text-main font-semibold border-main px-4 py-2 cursor-pointer">
-            See All Club
+            See All Events
           </button>
           </Link>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-5 gap-8 mt-8">
-          {allClubs?.map((club) => (
-            <ClubCard key={club._id} club={club} />
+          {allEvents?.map((event) => (
+            <EventCard key={event._id} event={event} />
           ))}
         </div>
       </div>
@@ -42,4 +42,4 @@ const FeaturedClubs = () => {
   );
 };
 
-export default FeaturedClubs;
+export default FeaturedEvents;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroBanner from '../components/HeroBanner';
 import FeaturedClubs from '../components/FeaturedClubs';
+import FeaturedEvents from '../components/FeaturedEvents';
 
 const Home = () => {
     return (
         <div>
             <HeroBanner />
             <FeaturedClubs />
+            <FeaturedEvents />
         </div>
     );
 };
