4dd63f2bda4b688aba3949797cd3a03895ed93a8
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-12-16 16:38:23 +0600

    added role based dashboard home

---
+++ src/pages/dashboard/clubManager/ClubManagerDashboardHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClubManagerDashboardHome = () => {
+    return (
+        <div>
+            club manager dashboard home
+        </div>
+    );
+};
+
+export default ClubManagerDashboardHome;
